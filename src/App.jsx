23d2460129f1c08d5a5bbb0c655bb85d97b00114
nayemkhan1999 <md.naiemkhan1999@gmail.com>
2024-03-16 06:43:37 +0600

    Create a project build by user Resturent Recipe and make a nav-bar

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import NavBar from './Component/NavBar'
+
+function App() {
+
+  return (
+    <>
+     
+      
+      <NavBar></NavBar>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/Component/NavBar.jsx
@@ -0,0 +1,46 @@
+
+const NavBar = () => {
+    return (
+        <div className="mx-20 mt-4">
+           <div className="navbar bg-base-100">
+  <div className="flex-1">
+    <a className="btn btn-ghost  font-lexend text-4xl font-bold">Recipe Calories</a>
+  </div>
+  <div className="mr-52">
+    <ul className="flex gap-8 items-center opacity-70">
+        <li>Home</li>
+        <li>Recipes</li>
+        <li>About</li>
+        <li>Search</li>
+    </ul>
+  </div>
+  
+  <div className="flex-none gap-2">
+    <div className="form-control ">
+    
+      <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto rounded-full" />
+    </div>
+    <div className="dropdown dropdown-end">
+      <div tabIndex="0" role="button" className="btn btn-ghost btn-circle avatar">
+        <div className="w-10 rounded-full">
+          <img alt="Tailwind CSS Navbar component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+        </div>
+      </div>
+      <ul tabIndex="0" className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+        <li>
+          <a className="justify-between">
+            Profile
+            <span className="badge">New</span>
+          </a>
+        </li>
+        <li><a>Settings</a></li>
+        <li><a>Logout</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
+        </div>
+    );
+};
+
+export default NavBar;
